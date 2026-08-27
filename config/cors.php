91a96010d1eb4baa6e76a2ad990fb4081a5cacb0
skouti-7788 <radwansakoti22@gmail.com>
2026-08-27 23:27:29 +0100

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -6,10 +6,9 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'http://localhost:5173',
-        'http://127.0.0.1:5173',
-        'http://localhost:5174',
-        'http://127.0.0.1:5174',
+         'menu-online.vercel.app',
+         'https://responsable-restaurant-sr5c.vercel.app'
+
     ],
 
     'allowed_origins_patterns' => [],
