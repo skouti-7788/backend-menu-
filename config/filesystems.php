90910b5bbe4c40b5ec54563197feb0f094422c39
radwan2020-l <radwansakoti77@gmail.com>
2026-09-05 00:04:05 +0100

    treat problm image

--- config/filesystems.php
+++ config/filesystems.php
@@ -48,8 +48,8 @@ return [
         ],
         'cloudinary' => [
             'driver' => 'cloudinary',
-            'key' => env('CLOUDINARY_KEY'),
-            'secret' => env('CLOUDINARY_SECRET'),
+            'key' => env('CLOUDINARY_API_KEY'),
+            'secret' => env('CLOUDINARY_API_SECRET'),
             'cloud' => env('CLOUDINARY_CLOUD_NAME'),
             'url' => env('CLOUDINARY_URL'),
             'secure' => (bool) env('CLOUDINARY_SECURE', true),
